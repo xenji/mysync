d6111baa872cea8099d2d2d4afe0e70458e819f7
Mario Mueller <mario@xenji.com>
2013-08-07 07:54:03 +0200

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Mario Mueller <mario@xenji.com> 2013
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "yaml.h"
+
+int main(int argc, const char* argv[]) {
+  return 0;
+}
